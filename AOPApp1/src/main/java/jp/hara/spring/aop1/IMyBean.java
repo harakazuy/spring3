96cff6d5bd74f3d0ecce18d53f4e20c484785691
kazuya.hara <kzyn400r@gmail.com>
2016-02-10 07:30:06 +0900

    Chapter5

---
+++ AOPApp1/src/main/java/jp/hara/spring/aop1/IMyBean.java
@@ -0,0 +1,8 @@
+package jp.hara.spring.aop1;
+
+public interface IMyBean<T> {
+	public void setDataObject(T obj);
+	public T getDataObject();
+	public void addData(Object obj);
+	public String toString();
+}
---
+++ AOPApp1/src/main/java/jp/hara/spring/aop1/MyBean1.java
@@ -0,0 +1,5 @@
+package jp.hara.spring.aop1;
+
+public class MyBean1 implements IMyBean<T> {
+
+}
